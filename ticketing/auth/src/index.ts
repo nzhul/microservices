69499e7bfba39be17f6fc1350f5c2db19764a4de
nzhul <dobromirivanov1@gmail.com>
2021-02-28 14:57:39 +0200

    add mongo container and work on user registration

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors"; //[dido] this package is used to allow throwing an errors in async functions in express context
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -23,6 +24,21 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("AUTH: Listening on port 3000!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("AUTH: Listening on port 3000!");
+  });
+};
+
+start();
