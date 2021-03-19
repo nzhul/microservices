6eadaf2395871068aba300052cf3cafb588de87b
nzhul <dobromirivanov1@gmail.com>
2021-03-19 13:02:07 +0200

    extract common code into npm package

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,7 +1,6 @@
 import express from "express";
 
-import { currentUser } from "../middlewares/current-user";
-import { requireAuth } from "../middlewares/require-auth";
+import { currentUser } from "@nzhul-tickets/common";
 
 const router = express.Router();
 
--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -4,8 +4,7 @@ import { body, validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
 import { Password } from "../services/password";
 import { User } from "../models/user";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@nzhul-tickets/common";
 
 const router = express.Router();
 
