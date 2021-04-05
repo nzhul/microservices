9ae5bed9417ac3188f2a41ea1c54c63e47d99417
nzhul <dobromirivanov1@gmail.com>
2021-04-05 10:36:21 +0300

    working on optimistic concurrency issues

--- ticketing/orders/src/events/listeners/ticket-updated-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,7 +8,8 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
-    const ticket = await Ticket.findById(data.id);
+    const ticket = await Ticket.findByEvent(data);
+
     if (!ticket) {
       throw new Error("Ticket not found");
     }
@@ -16,7 +17,7 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
     const { title, price } = data;
     ticket.set({ title, price });
     await ticket.save();
-    
+
     msg.ack();
   }
 }
